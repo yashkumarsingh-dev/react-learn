59d5213edc0c304011370ed30b3471168c01880b
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-04-09 18:45:39 +0530

    cm

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
 
-  let [counter,setCounter] = useState(15)
+  let [counter,setCounter] = useState(0)   // counter = 0
 
   const addvalue = () => {
     counter = counter + 1
