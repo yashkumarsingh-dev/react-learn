8771b15ad7eb1c94cff6fd3b34b6fbbb5bfb217d
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-04-09 18:35:35 +0530

    tailwindcss

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,12 +6,16 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    name: "Yash",
+    age: 20
+  }
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card />
-      <Card />
+      <Card username="Yash" btnText="Click Me"/>
+      <Card username="Sahil" />
     </>
   )
 }
