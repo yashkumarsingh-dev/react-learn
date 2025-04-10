e9fee3ad6e97390b33539f83f61da2620a4c3df6
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-04-10 18:23:15 +0530

    tc

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -5,8 +5,6 @@ import "./App.css"
 function App() {
   const [color,setColor] = useState("olive");
 
-  
-
   return (
    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}
    >
