0e108e23cfdb38bb35569b0d006385b07d286f9e
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-04-10 18:22:18 +0530

    bgChange using taileindcss

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import "./App.css"
+
+
+function App() {
+  const [color,setColor] = useState("olive");
+
+  
+
+  return (
+   <div className="w-full h-screen duration-200" style={{backgroundColor: color}}
+   >
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">Test
+      <button className="btn" onClick={() => setColor("red")}>Red</button>
+      <button className="btn" onClick={() => setColor("green")}>Green</button>
+      <button className="btn" onClick={() => setColor("blue")}>Blue</button>
+      <button className="btn" onClick={() => setColor("yellow")}>Yellow</button>
+      <button className="btn" onClick={() => setColor("black")}>Black</button>
+
+    </div>
+
+   </div>
+  // <div style={{ width: "100vw", height: "100vh", backgroundColor: "olive" }}>
+  // </div>
+
+  )
+}
+
+export default App
