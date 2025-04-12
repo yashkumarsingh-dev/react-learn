f7551eaa98a71c7e0a801ffab38b922b896f5c37
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-04-12 16:33:47 +0530

    password generator

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,20 +6,29 @@ function App() {
   const [color,setColor] = useState("olive");
 
   return (
-   <div className="w-full h-screen duration-200" style={{backgroundColor: color}}
-   >
-    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">Test
-      <button className="btn" onClick={() => setColor("red")}>Red</button>
-      <button className="btn" onClick={() => setColor("green")}>Green</button>
-      <button className="btn" onClick={() => setColor("blue")}>Blue</button>
-      <button className="btn" onClick={() => setColor("yellow")}>Yellow</button>
-      <button className="btn" onClick={() => setColor("black")}>Black</button>
-
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+      </div>
     </div>
-
-   </div>
-  // <div style={{ width: "100vw", height: "100vh", backgroundColor: "olive" }}>
-  // </div>
 
   )
 }
