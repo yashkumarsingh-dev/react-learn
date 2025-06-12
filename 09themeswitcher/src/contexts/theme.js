0153e022ab81f042755c9315d42b43afc164b6f8
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-06-12 17:13:25 +0530

    themeswitcher commit

---
+++ 09themeswitcher/src/contexts/theme.js
@@ -0,0 +1,13 @@
+import { createContext, use, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider 
+
+export default function useTheme(){
+    return useContext(ThemeContext);
+}
