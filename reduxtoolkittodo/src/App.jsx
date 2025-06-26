d166ba50b78c25ffa4a1bb027c98dab33e0570c0
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-06-26 09:09:36 +0530

    redux toolkit

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -1,54 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Learn Redux Toolkit" }],
+};
 
 export const todoSlice = createSlice({
   name: "todo",
-  initialState: {
-    todos: [],
-    status: "idle", // 'idle' | 'loading' | 'succeeded'
-    error: null,
-  },
+  initialState,
   reducers: {
     addTodo: (state, action) => {
-      state.todos.push({
-        id: Date.now(),
-        text: action.payload,
-        completed: false,
-      });
-    },
-    toggleTodo: (state, action) => {
-      const todo = state.todos.find((todo) => todo.id === action.payload);
-      if (todo) {
-        todo.completed = !todo.completed;
-      }
+      const todo = {
+        id: nanoid(),
+        text: action.payload.text,
+      };
+      state.todos.push(todo);
     },
     removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      state.todos = state.todos.filter((todo) => todo.id != action.payload);
     },
-    clearTodos: (state) => {
-      state.todos = [];
-    },
-    setTodos: (state, action) => {
-      state.todos = action.payload;
-    },
-    setStatus: (state, action) => {
-      state.status = action.payload;
-    },
-    setError: (state, action) => {
-      state.error = action.payload;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase("todos/fetchTodos/pending", (state) => {
-        state.status = "loading";
-      })
-      .addCase("todos/fetchTodos/fulfilled", (state, action) => {
-        state.status = "succeeded";
-        state.todos = action.payload;
-      })
-      .addCase("todos/fetchTodos/rejected", (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
   },
 });
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
